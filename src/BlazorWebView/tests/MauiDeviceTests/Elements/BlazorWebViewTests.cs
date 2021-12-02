eff009e09533f61d1f7dd4fb087f5e3fac3c668e
Eilon Lipton <Eilon@users.noreply.github.com>
2021-12-01 16:16:58 -0800

    Initial projects and settings for BlazorWebView device tests

---
+++ src/BlazorWebView/tests/MauiDeviceTests/Elements/BlazorWebViewTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebView.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Handlers;
+using Xunit;
+
+namespace Microsoft.Maui.DeviceTests
+{
+	[Category(TestCategory.BlazorWebView)]
+	public partial class BlazorWebViewTests : HandlerTestBase
+	{
+		[Fact]
+		public void BlazorFailing()
+		{
+			var bwv = new BlazorWebView()
+			{
+				HostPage = "value",
+			};
+
+			Assert.Equal("value2", bwv.HostPage);
+		}
+
+		[Fact]
+		public void BlazorPassing()
+		{
+			var bwv = new BlazorWebView()
+			{
+				HostPage = "value",
+			};
+
+			Assert.Equal("value", bwv.HostPage);
+		}
+	}
+}
